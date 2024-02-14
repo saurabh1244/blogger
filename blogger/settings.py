5b0a58f2978d11b956f28c89c25fdf89a7ef22ff
Saurabh chandra <126794525+saurabh1244@users.noreply.github.com>
2024-02-14 22:51:57 +0530

    Update settings.py

--- blogger/settings.py
+++ blogger/settings.py
@@ -8,6 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+import os
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-icx$hr^)qz9e5!^8y43%&zzw1h#*#!x@wtudld4fub1+rlfl=h'
