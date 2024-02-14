8d063b966d411aa9f9dc119f56082e16091790eb
Saurabh chandra <126794525+saurabh1244@users.noreply.github.com>
2024-02-14 22:41:05 +0530

    Update settings.py

--- blogger/settings.py
+++ blogger/settings.py
@@ -10,10 +10,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-icx$hr^)qz9e5!^8y43%&zzw1h#*#!x@wtudld4fub1+rlfl=h'
+# SECRET_KEY = 'django-insecure-icx$hr^)qz9e5!^8y43%&zzw1h#*#!x@wtudld4fub1+rlfl=h'
+SECRET_KEY  = os.getenv('SECRET_KEY','django-insecure-icx$hr^)qz9e5!^8y43%&zzw1h#*#!x@wtudld4fub1+rlfl=h')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.getenv('DEBUG','True') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
