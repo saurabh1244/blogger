73201f2fa945e9cb04adb4e82370eb190cfdecdf
Saurabh chandra <126794525+saurabh1244@users.noreply.github.com>
2024-02-14 23:11:07 +0530

    Update settings.py

--- blogger/settings.py
+++ blogger/settings.py
@@ -37,6 +37,8 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    
+    'whitenoise.middleware.common.WhiteNoiseMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
