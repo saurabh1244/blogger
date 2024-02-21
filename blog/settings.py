f1e653d19024de67f9fb345f8940919ddb2cf585
Saurabh chandra <126794525+saurabh1244@users.noreply.github.com>
2024-02-22 03:29:03 +0530

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -126,6 +126,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+DISABLE_COLLECTSTATIC = True
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
